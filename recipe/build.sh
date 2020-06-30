13d9119a4ecf69faad95b6b8a75b8dd03f5d3642
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-06-30 18:27:09 +0200

    Use OpenSSL static library

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib     \
       -DXEUS_DISABLE_ARCH_NATIVE=ON  \
+      -DOPENSSL_USE_STATIC_LIBS=ON   \
       $SRC_DIR
 
 make install
